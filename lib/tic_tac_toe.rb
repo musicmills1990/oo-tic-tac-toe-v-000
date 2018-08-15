be6a3803838aae8be985dfac722cd2b6c14e28b8
Marc Mills <mdmills1990@gmail.com>
2018-08-15 15:03:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,7 +7,7 @@ end
 
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-  def display_board(board)
+  def display_board
 
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -17,18 +17,18 @@ end
   end
 
 
-  def input_to_index(user_input)
+  def input_to_index
     user_input.to_i - 1
   end
 
 
-  def move(board, index, player)
+  def move(index, player)
     @board[index] = player
   end
 
 
 
-  def position_taken?(board, location)
+  def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end
 
